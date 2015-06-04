fc9feb4a37de11821114b79edd66fcb7adfa99c6
Marla Brizel <marlabrizel@gmail.com>
2015-06-04 13:00:37 -0600

    fix failing test

--- test/outgoing_file_test.rb
+++ test/outgoing_file_test.rb
@@ -7,7 +7,7 @@ class OutgoingFileTest < Minitest::Test
   def test_it_puts_encrypted_message_into_file
     encrypted_message = "43p3s5b"
     outgoing = OutgoingFile.new("file.txt")
-    outgoing.create_file(encrypted_message)
+    outgoing.encrypt_file(encrypted_message)
     assert_equal encrypted_message, "43p3s5b"
   end
 
