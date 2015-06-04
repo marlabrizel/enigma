ecea193a5d89cc1211499127886c1618713ca4aa
Marla Brizel <marlabrizel@gmail.com>
2015-06-04 00:13:28 -0600

    delete commented out pseudocode

--- lib/encryptor.rb
+++ lib/encryptor.rb
@@ -15,7 +15,7 @@ class Encryptor
     out_message = split_message.map do |item|
       results = []
       item.each_with_index do |char, index|
-        value = char_map.fetch(char) # => value
+        value = char_map.fetch(char)
         # first evaaaa metaprogramming!!!!
         rotate = (value + @calc.send(rotation_for_index(index))) % 39
         results.push char_map.key(rotate)
@@ -24,15 +24,7 @@ class Encryptor
       results.join("")
     end
     out_message.join("")
-    #split message into arrays of four characters- each_slice(4)
-    #use hash.fetch to look up the value for each letter in the array - probably need
-    # "message" => [["m", "e", "s", "s"], ["a", "g", "e"]] => [[]]
-    # another enumerable to iterate through
-    #add the lookup value to the correct x_final
-    # "m" => 13 + a_final(2) = 15
-    #use hash.key(summed_value) to get new key
-    # hash.key(15) => "o"
-    # push new key into encrypted array
+
   end
 
   protected
@@ -87,15 +79,3 @@ class Encryptor
   end
 
 end
-
-# divide message into arrays of four
-# The first character is rotated forward by a_final - char[0]
-# The second character is rotated forward by b_final - char[1]
-# The third character is rotated forward by c_final - char[2]
-# The fourth character is rotated forward by d_final - char[3]
-
-# Create an encryptor object in your runner
-# What information would the encryptor need to be "setup" and
-# ready to encrypt messages? Pass that in.
-# Call an encrypt method and pass in a string message.
-# Get back the encrypted version.
